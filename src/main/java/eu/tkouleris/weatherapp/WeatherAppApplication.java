c682f0194f30ba2f3d7d81128e517131f14ae6ff
Thodoris Kouleris <kurokokoro@windowslive.com>
2022-01-16 17:37:06 +0200

    first commit

---
+++ src/main/java/eu/tkouleris/weatherapp/WeatherAppApplication.java
@@ -0,0 +1,24 @@
+package eu.tkouleris.weatherapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class WeatherAppApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(WeatherAppApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+//    @Bean
+//    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+//        return builder.build();
+//    }
+}
---
+++ src/main/java/eu/tkouleris/weatherapp/controller/WeatherController.java
@@ -0,0 +1,34 @@
+package eu.tkouleris.weatherapp.controller;
+
+import eu.tkouleris.weatherapp.jsonModel.WeatherModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+
+@Controller
+@RequestMapping("/weather/")
+public class WeatherController {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping(path = "forecast")
+    public ResponseEntity<Object> getWeather(RestTemplateBuilder builder) {
+//        https://www.youtube.com/watch?v=B792AiYpb50
+        String url = "https://api.openweathermap.org/data/2.5/forecast?id=524901&&appid=93265401429e6e79657b6e0b6d6acb96";;
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);//or any other required
+        HttpEntity entity = new HttpEntity(headers);
+        ResponseEntity<String> response = restTemplate.exchange(
+                url,
+                HttpMethod.GET,
+                entity,
+                String.class
+        );
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
+}
