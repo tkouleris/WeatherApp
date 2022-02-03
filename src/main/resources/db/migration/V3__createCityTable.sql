8e80c61bcde103d34846b578f44769baf2166850
Thodoris Kouleris <kurokokoro@windowslive.com>
2022-02-03 19:08:01 +0200

    Update V3__createCityTable.sql

--- src/main/resources/db/migration/V3__createCityTable.sql
+++ src/main/resources/db/migration/V3__createCityTable.sql
@@ -1,7 +1,9 @@
 CREATE TABLE city
 (
-    id  bigint unsigned auto_increment primary key,
-    city_name varchar(255) not null,
-    country varchar(255) not null,
-    owm_id bigint unsigned
+    id        bigint unsigned auto_increment primary key,
+    city_name varchar(255)    not null,
+    country   varchar(255)    not null,
+    owm_id    bigint unsigned null
 );
+
+CREATE INDEX city_owm_id_index ON city (owm_id);
